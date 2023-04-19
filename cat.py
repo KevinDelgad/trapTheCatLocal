c9d51471b1167801c5d45625ecd03974a104a12f
Kevin Delgado <kevindel891@gmail.com>
2023-04-19 14:53:28 -0700

    add initial pathList which represent which nodes each node can reach not

--- cat.py
+++ cat.py
@@ -1,7 +1,29 @@
 class Cat:
-    def __init__(self):
+
+    movePaths = {}    
+
+    def __init__(self, validSpaces):
         self.locationX = "5"
         self.locationY = "5"
+        for elems in validSpaces:
+            y = elems.split('x')[0]
+            x = elems.split('x')[1]
+            possibleTiles = []
+            if(x == '0' or x == '10' or y == '0' or y == '10'):
+                continue
+            
+            possibleTiles.append(str(int(y) - 1) + 'x' + x)
+            possibleTiles.append(str(int(y) - 1) + 'x' + str(int(x) + 1))
+
+            possibleTiles.append(y + 'x' + str(int(x) - 1))
+            possibleTiles.append(y + 'x' + str(int(x) + 1))
+
+            possibleTiles.append(str(int(y) + 1) + 'x' + x)
+            possibleTiles.append(str(int(y) + 1) + 'x' + str(int(x) + 1))            
+
+            self.movePaths[elems] = possibleTiles
+
+        print(self.movePaths)
 
     def calculateNextMove(self, validSpaces):
         print(self.locationX)
@@ -10,5 +32,5 @@ class Cat:
     def checkTrapped(self, validSpaces):
         pass
 
-    def checkWon(self)
+    def checkWon(self):
         pass
